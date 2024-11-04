eeb38a95225ace4ec384d93dd21d9f7eeb4d39f1
AbhiramiTS <abhiramits004@gmail.com>
2024-11-04 14:03:35 +0530

    final changes

--- job_analysis/job_analysis/settings.py
+++ job_analysis/job_analysis/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4yn#9%x3x6r1c%czy7*=kto^*obz*0++yn6g*(*q%(uqvwi_xd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*","localhost"]
 
 
 # Application definition
@@ -122,13 +122,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = 'static/'
 
-MEDIA_URL = '/images/'
-
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
-
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
